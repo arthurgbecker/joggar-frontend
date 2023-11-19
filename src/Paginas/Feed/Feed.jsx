466fa3933a12471f27fa6975bb34058cad47c92d
arthurgbecker <arthurgbecker@gmail.com>
2023-11-18 22:12:57 -0300

    18/11 - Tentando fazer request de evento

--- src/Paginas/Feed/Feed.jsx
+++ src/Paginas/Feed/Feed.jsx
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faHouse, faUserGroup, faCirclePlus, faBell, faUser } from '@fortawesome/free-solid-svg-icons';
 library.add(faHouse, faUserGroup, faCirclePlus, faBell, faUser);
 
 import axios from 'axios';
-import { useEffect } from 'react';
-
 import styles from './Feed.module.css'
 
 const Feed = () => {
+
+    const [eventos, setEventos] = useState([]);
+
+    useEffect(() => {
+        loadEventos();
+    }, []);
+
+    const loadEventos = async () => {
+        const result = await axios.get("http://localhost:8080/eventos");
+        setEventos(result.data);
+        console.log(result.data);
+    }
+
+    // useEffect(() => {
+    //     const fetchEventos = async () => {
+    //         try {
+    //             const response = await axios.get('http://localhost:3306/eventos');
+    //             if (response.data.length > 0) {
+    //                 setEventos(response.data);
+    //             } else {
+    //                 console.log('Nenhum evento encontrado.');
+    //             }
+    //         } catch (error) {
+    //             console.error('Erro ao obter eventos:', error);
+    //         }
+    //     };
+
+    //     fetchEventos();
+    // }, []);
+
     const dropdownOptions = [
         ["Opção 1", "Opção 2", "Opção 3"],
         ["Opção A", "Opção B", "Opção C"],
@@ -18,18 +46,18 @@ const Feed = () => {
         ["Categoria 1", "Categoria 2", "Categoria 3"],
     ];
 
-    const getAxios = async () => {
-        console.log("teste da api com get")
-    };
+    // const getAxios = async () => {
+    //     console.log("teste da api com get")
+    // };
 
-    const deleteAxios = async () => {
-        console.log("teste da api com delete")
-    };
+    // const deleteAxios = async () => {
+    //     console.log("teste da api com delete")
+    // };
 
-    useEffect(() => {
-        getAxios();
-        deleteAxios();
-    }, []);
+    // useEffect(() => {
+    //     getAxios();
+    //     deleteAxios();
+    // }, []);
 
     return (
         <div className={styles.mainFeed}>
@@ -77,7 +105,14 @@ const Feed = () => {
             </div>
 
             <div className={styles.contentFeed}>
-                {/* Conteúdo principal */}
+
+                {eventos.map((evento) => (
+                    <div key={evento.id} className={styles.card}>
+                        <h3>{evento.tituloEvento}</h3>
+                        <p>Data: {evento.dataEvento}</p>
+                        <p>Descrição: {evento.descricaoEvento}</p>
+                    </div>
+                ))}
             </div>
         </div>
     );
